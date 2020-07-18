930e3a74bc74cd2d3197d2954e54d389cfa3d031
wykngup <65691570+wykngup@users.noreply.github.com>
2020-07-18 14:28:15 +0800

    一个文件
    第一次用github上传文件

---
+++ timethread.cpp
@@ -0,0 +1,55 @@
+﻿#include "timethread.h"
+
+timeThread::timeThread(QObject *parent) :
+    QThread(parent)
+{
+
+}
+//接受发送来的src和target信息
+void timeThread::recvInfoSlot(QString src, QString target)
+{
+    this->target=target;
+    this->src=src;
+}
+
+void timeThread::run( ){
+    qDebug() << "调试4:";
+
+    //建个文件信息类
+    QFileInfo info(src);
+    //获得文件大小
+    qint64 bytes = info.size();
+
+    QFile fileRead(src);
+    fileRead.open(QIODevice::ReadOnly);
+
+    QFile fileWrite(target);
+    fileWrite.open(QIODevice::WriteOnly);
+
+    QByteArray buf;
+    double value=0;
+    int i=0;
+
+
+    while(!fileRead.atEnd())
+    {
+//      qDebug()<<"1";
+        buf = fileRead.read(1024);
+
+        fileWrite.write(buf);
+        i++;
+        value= double(1024*i)/double(bytes)*100;
+        emit sendValueSign(value,i);
+
+    }
+
+    i=0;
+    fileWrite.flush();
+    fileWrite.close();
+    fileRead.close();
+
+
+
+
+}
+
